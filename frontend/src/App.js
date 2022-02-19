3da826f38c18a7927833aafc35fc44939c1bec16
whasnat <w.hasnat94@gmail.com>
2022-02-19 20:37:07 +0600

    V-0.2-Added frontend structure and global styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,30 @@
 import React from "react";
+import "./App.scss";
+
+import {
+  About,
+  Footer,
+  Header,
+  Experience,
+  Skills,
+  Testimonial,
+  Work,
+} from "./container";
+import { Navbar } from "./components";
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Skills />
+      <Experience />
+      <Work />
+      <Testimonial />
+      <Footer />
+    </div>
   );
-}
+};
 
 export default App;
