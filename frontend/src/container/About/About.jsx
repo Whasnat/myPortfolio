9fb590f170937a4b8755045f259fec8a0911366c
whasnat <w.hasnat94@gmail.com>
2022-02-27 15:29:13 +0600

    About section done,work section is under progress

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -44,28 +44,31 @@ const About = () => {
 
   return (
     <>
-      <h2 className="head-text">
-        I Love <span>Fried Chicken</span> <br /> and <span>Building stuff</span>
-      </h2>
+      <div id="about">
+        <h2 className="head-text top-text">
+          I Love <span>Fried Chicken</span> <br /> and{" "}
+          <span>Building stuff</span>
+        </h2>
 
-      <div id="about" className="app__profiles">
-        {abouts.map((about, index) => (
-          <motion.div
-            whileInView={{ opacity: 1 }}
-            whileHover={{ scale: 1.1 }}
-            transition={{ duration: 0.5, type: "tween" }}
-            className="app__profile-item"
-            key={about.title + index}
-          >
-            <img src={urlFor(about.imgUrl)} alt={about.title} />
-            <h2 className="bold-text" style={{ marginTop: 20 }}>
-              {about.title}
-            </h2>
-            <p className="p-text" style={{ marginTop: 10 }}>
-              {about.description}
-            </p>
-          </motion.div>
-        ))}
+        <div className="app__profiles">
+          {abouts.map((about, index) => (
+            <motion.div
+              whileInView={{ opacity: 1 }}
+              whileHover={{ scale: 1.1 }}
+              transition={{ duration: 0.5, type: "tween" }}
+              className="app__profile-item"
+              key={about.title + index}
+            >
+              <img src={urlFor(about.imgUrl)} alt={about.title} />
+              <h2 className="bold-text" style={{ marginTop: 20 }}>
+                {about.title}
+              </h2>
+              <p className="p-text" style={{ marginTop: 10 }}>
+                {about.description}
+              </p>
+            </motion.div>
+          ))}
+        </div>
       </div>
     </>
   );
