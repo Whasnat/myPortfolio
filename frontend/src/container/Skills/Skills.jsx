4aa5da8edd96f97e0cc35763b978401a0f558235
whasnat <w.hasnat94@gmail.com>
2022-03-01 15:27:19 +0600

    V - 0.9 Skils added

--- frontend/src/container/Skills/Skills.jsx
+++ frontend/src/container/Skills/Skills.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import React from "react";
 import "./Skills.scss";
 
@@ -9,7 +10,11 @@ const Skills = () => {
           Here are some <span>Technologies</span> I know
         </h3>
 
-        <div className="app__skill_"></div>
+        <div className="app__skill-item">
+          <motion.div whileInView={[{ opacity: [0, 1] }]}
+            transition={{ }}
+          ></motion.div>
+        </div>
       </div>
     </>
   );
