b543f71062d2e8350d7666227661f232c12c8204
whasnat <w.hasnat94@gmail.com>
2022-03-09 06:00:53 +0600

    V-0.12 - Testimonial

--- frontend/src/container/Testimonial/Testimonial.jsx
+++ frontend/src/container/Testimonial/Testimonial.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import './Testimonial.scss';
-
+import { motion } from "framer-motion";
+import React from "react";
+import "./Testimonial.scss";
+import { client, AppWrap } from "../../wrapper";
 const Testimonial = () => {
   return (
-    <div>Testimonial</div>
-  );
-}
+    <>
+      <div id="testimonial">
+        <h3 className="head-text">
+          <span>Here</span> is what people say about <span>Me</span>
+        </h3>
 
-export default Testimonial;
+        <div className="app__testimonial-container">
+          <motion.div
+            whileInView={{ opacity: [0, 1] }}
+            transition={{ duration: 0.5 }}
+            className="app__testimonial-item app__flex"
+          >
+            <div className="app__testimonial-list"></div>
+          </motion.div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AppWrap(Testimonial, "testimonial");
