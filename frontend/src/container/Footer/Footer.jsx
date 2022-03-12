7a91355d74678e4b755f9adf5014914524f34c91
whasnat <w.hasnat94@gmail.com>
2022-03-12 18:13:41 +0600

    V0.14-footer section

--- frontend/src/container/Footer/Footer.jsx
+++ frontend/src/container/Footer/Footer.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
-import './Footer.scss';
+import React, { useState } from "react";
+import { images } from "../../container";
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { client } from "../../client";
+
+import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <h2 className="head-text">Get in touch</h2>
+      <div className="app__footer-cards">
+        <div className="app__footer-card">
+          <img src={images.email} alt="email" />
+          <a href="mailto:w.hasnat94@gmail.com"></a>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Footer
+export default AppWrap(
+  MotionWrap(Footer, "app__footer"),
+  "contact",
+  "app__whitebg"
+);
